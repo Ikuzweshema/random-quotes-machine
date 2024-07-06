56e0a245a8f8db9e8aaf34aaf215a652130d1954
Shema Elisa <163114861+Ikuzweshema@users.noreply.github.com>
2024-07-06 14:59:49 +0200

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-
-import { useState } from 'react';
-import './App.css';
-import qoutes from "./assets/qoutes.json"
+import { useState } from "react";
+import "./App.css";
+import qoutes from "./assets/qoutes.json";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 function App() {
-
   function getRandomQoute() {
-    return qoutes[Math.floor(Math.random()*qoutes.length)];
+    return qoutes[Math.floor(Math.random() * qoutes.length)];
   }
-  const[qoute,setQoute]=useState(getRandomQoute())
-  
-  return <div>
-      <div id='qoute-box'>
-        <div className='qoute-content'>
-           <h2 id='text'>{qoute.quote}</h2>
-           <h4 id=''>{qoute.author}</h4>
+  const [qoute, setQoute] = useState(getRandomQoute());
+
+  return (
+    <div className=" background">
+      <div id="qoute-box">
+        <div className="qoute-content">
+          <FaQuoteLeft size={30} style={{ marginLeft: "10px" }} />
+          <h2 id="text">{qoute.quote}</h2>
+          <FaQuoteRight size={30} style={{ marginRight: "10px" }} />
+          <h4 id=" author">{qoute.author}</h4>
+        </div>
+        <div className="buttons">
+           <a href="twitter.com/intent/tweet"></a>
         </div>
       </div>
-  </div>
-
+    </div>
+  );
 }
 
-export default App
+export default App;
